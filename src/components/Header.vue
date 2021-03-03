542c2626390165567ff41b238adda199ad05e0c2
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-03 14:52:57 -0600

    Added githib source code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <div>
     <b-nav tabs>
       <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item><a href="https://github.com/vsahinid/vue-quiz">GitHub</a></b-nav-item>
       <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
     </b-nav>
   </div>
