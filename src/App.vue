16163e76689c640c6de9edf19846dd7554414593
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-03 13:55:59 -0600

    Added numcorrent and numtotal to header component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    <Header/>
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
 
     <b-container class="bv-example-row">
-    <b-row>
-      <b-col sm="6" offset="3">
-        <QuestionBox/>
-      </b-col>
-    </b-row>
-  </b-container>
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -20,13 +28,43 @@ export default {
   name: 'App',
   components: {
     Header, QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next(){
+      this.index++
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++
+      }
+      this.numTotal++
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+      method: 'get'
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results
+      })
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -34,3 +72,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -2,17 +2,36 @@
     <div class="question-box-container">
         <b-jumbotron>
             <template #lead>
-            Add question here?
+            {{ currentQuestion.question }}
             </template>
 
             <hr class="my-4">
 
-            <p>
-            List of answers
+            <p v-for="answer in answers" :key="answer">
+                {{answer}}
             </p>
 
-            <b-button variant="primary" href="#">Do Something</b-button>
-            <b-button variant="success" href="#">Do Something Else</b-button>
+            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button variant="success" @click="next" href="#">Next</b-button>
         </b-jumbotron>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function,
+    },
+    mounted(){
+        console.log(this.currentQuestion)
+    },
+    computed: {
+        answers(){
+            let answers = [...this.currentQuestion.incorrent_answers]
+            answers.push(this.currentQuestion.correct_answer)
+            return answers;
+        }
+    }
+}
+</script>
