e0243722042dd1bb5f95bd88bb112d6ef71b6e30
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-03 12:52:53 -0600

    added questionbox

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>Question Box</h1>
+    <div class="question-box-container">
+        <b-jumbotron>
+            <template #lead>
+            Add question here?
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+            List of answers
+            </p>
+
+            <b-button variant="primary" href="#">Do Something</b-button>
+            <b-button variant="success" href="#">Do Something Else</b-button>
+        </b-jumbotron>
+    </div>
 </template>
